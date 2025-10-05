1b1fe1eeeada55347f413b517cf3fabad9291d2c
ProhaszkaAbel <prohaszka.abel@blathy.info>
2025-10-05 14:21:31 +0200

    Added empty algorithms

---
+++ Algorithms/Recursive/Algorithms/StaircaseProblem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.Recursive.Algorithms
+{
+	public static partial class RecursiveAlgorithms
+	{
+		public static int[] StaircaseProblem()
+		{
+			// allow that a person can climb either 1 or ... n steps at a time (by params array!)
+			throw new NotImplementedException();
+		}
+	}
+}
